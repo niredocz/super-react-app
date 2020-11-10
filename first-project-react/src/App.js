3a6d4819ff5383cf5d9a45850dd5511264909d91
Niredocz <daffa.rogers@gmail.com>
2020-11-10 14:56:07 +0700

    add todo components

--- first-project-react/src/App.js
+++ first-project-react/src/App.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
+import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
-import './App.css';
+
 
 function App() {
   const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return(
     <div className="App">
       <header>
