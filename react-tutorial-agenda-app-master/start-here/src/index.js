1a6c77418350e9c76fe3ee0f211bc162c152cedb
Niredocz <daffa.rogers@gmail.com>
2020-11-10 11:18:42 +0700

    first react commit

--- react-tutorial-agenda-app-master/start-here/src/index.js
+++ react-tutorial-agenda-app-master/start-here/src/index.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
-
-
-
-//JSX
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+class App extends Component {
+    state = {};
+    render() {
+        return (
+            <React.Fragment>
+            <MDBContainer>
+                <MDBRow>
+                <MDBCol lg="6">Left column</MDBCol>
+                <MDBCol lg="6">Right column</MDBCol>
+                </MDBRow>
+            </MDBContainer>
+            </React.Fragment>
+        );
+    }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
